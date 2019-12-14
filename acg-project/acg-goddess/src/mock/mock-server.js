ea611d2c6ff40ea3d46fefc5b535768fe9c535dc
“meng <email@ec101o062275859@qq.comgit config --global user.email email@ec101o062275859@qq.com>
2019-12-14 11:07:51 +0800

    合并redux mock 及静态页面

---
+++ acg-project/acg-goddess/src/mock/mock-server.js
@@ -0,0 +1,94 @@
+import Mock from 'mockjs'
+import datas from './data'
+import qs from 'qs'
+//用户的操作
+/**
+ * userName,isAdmin,password  ==需要传入
+ * id --唯一表示
+ * userName --用户名
+ * isAdmin  --是否是管理员
+ * passWored --用户密码  ---不用传入，调用接口的时候
+ * userImg  --用户图像   ---不用传入，图片有固定图片
+ */
+//反回所有数据data 
+Mock.mock('/data', () => {
+  // localStorage.setItem('data', JSON.stringify(data))
+  const data = JSON.parse(localStorage.getItem('data')) || datas
+  return data
+})
+//添加用户
+//userName,isAdmin,password  ==需要传入
+Mock.mock('/addUser', (options) => {
+  //获取添加的数据
+  let user = qs.parse(options.body)
+  const data = JSON.parse(localStorage.getItem('data')) || datas
+  // 添加固定属性
+  user.id = +new Date()
+  user.passWored = 123456
+  user.userImg = data.user[0].userImg
+  //添加数据到data中
+  data.user.push(user)
+  
+  localStorage.setItem('data', JSON.stringify(data))
+  return data
+})
+//删除用户
+//传入Id444
+Mock.mock('/removeUser', (options) => {
+  let id = qs.parse(options.body)
+  const data = JSON.parse(localStorage.getItem('data')) || datas
+  //筛序下的数据
+  data.user = data.user.filter(item => item.id !== +id.id)
+  localStorage.removeItem('data')
+  localStorage.setItem('data', JSON.stringify(data))
+  return data
+})
+//获取用户
+//文章的操作
+/** 传入 title author createTime mian
+ * id --唯一标识  ===时间戳
+ * title ---标题
+ * author   ---作者        
+ * createTime  --创建时间  
+ * main ---文章内容
+ */
+//添加文章
+// 传入 title author createTime mian
+Mock.mock('/addArt', (options) => {
+  let art = qs.parse(options.body)
+  //先从本地缓存获取，没有从本文件夹
+  const data = JSON.parse(localStorage.getItem('data')) || datas
+  art.id = +new Date()
+  data.article.push(art)
+  localStorage.setItem('data', JSON.stringify(data))
+  return data
+})
+//删除文章 == id
+Mock.mock('/removeArt', (options) => {
+  let id = qs.parse(options.body)
+  const data = JSON.parse(localStorage.getItem('data')) || datas
+  data.article = data.article.filter(item => item.id !== +id.id)
+  localStorage.removeItem('data')
+  localStorage.setItem('data', JSON.stringify(data))
+  return data
+})
+
+/**  commentUser commentTime commentMain commentArtId
+ *comment
+ * commentUser   ---评论人
+ * commentTime   ---时间
+ * commentMain   ---评论内容
+ * commentArtId  ---文章id  根据id找到对应的id 
+ * id            ---唯一标识  
+ */
+// 删除评论
+Mock.mock('/removeCom', (options) => {
+
+  let id = qs.parse(options.body)
+  const data = JSON.parse(localStorage.getItem('data')) || datas
+  data.comment = data.comment.filter(item => item.id !== +id.id)
+  localStorage.removeItem('data')
+  localStorage.setItem('data', JSON.stringify(data))
+  return data
+})
+
--- acg-project/acg-goddess/src/redux/reducers.js
+++ acg-project/acg-goddess/src/redux/reducers.js
@@ -1,14 +1,62 @@
-// 包含了多个的reducers,更新/修改状态数据的函数
-// 引入action的type
-import { SAVE_USER } from './action-types.js'
-// 引入redux
-import { combineReducers } from "redux"
-// 引入storage.js 文件
-import { setItem, getItem, removeItem } from "../utils/storage.js"
+import { GET_DATA, ADD_USER, REMOVE_USER, ADD_ART, REMOVE_ART, REMOVE_COM，SAVE_USER  } from './action-types'
+
+import { combineReducers } from 'redux'
+//数据的获取
+function data(prevState = [], action) {
+  switch (action.type) {
+    case GET_DATA:
+      return action
+    default:
+      return prevState
+  }
+}
+//用户的操作
+function user(prevState = [], action) {
+  switch (action.type) {
+    case ADD_USER:
+      return [...prevState, action.data]
+    case REMOVE_USER:
+      return action.data
+    default:
+      return prevState
+  }
+}
+//文章的操作
+function art(prevState = [], action) {
+  switch (action.type) {
+    case ADD_ART:
+      return [...prevState, action.data]
+    case REMOVE_ART:
+      return action.data
+    default:
+      return prevState
+  }
+}
+//评论的操作
+function com(prevState = [], action) {
+  switch (action.type) {
+    case REMOVE_COM:
+      return action.data
+    default:
+      return prevState
+  }
+}
+//获取数据的时候最好用data  ，，不对数据进行修改的时候redux中的user,art,com为空
+//mock没写对应的的单个数据的获取方法，数据在data中
+//当你对user,art，com 就可以获取在redux中单独的获取了
+export default combineReducers({
+  data,
+  user,
+  art,
+  com
+})
+
+// 登录
 const initUser = {
   user:getItem('user') || {},
   token:getItem("token")||{}
 }
+
 function user(prevState=initUser,action){
   // 判断type
   switch (action.type) {
