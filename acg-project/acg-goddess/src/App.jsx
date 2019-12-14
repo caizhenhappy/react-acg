122f176af0a4e3b972856c233d5b07e59d2a4638
“meng <email@ec101o062275859@qq.comgit config --global user.email email@ec101o062275859@qq.com>
2019-12-14 12:10:30 +0800

    所有代码合并

--- acg-project/acg-goddess/src/App.jsx
+++ acg-project/acg-goddess/src/App.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Link, Switch, Redirect, Route } from 'react-router-dom'
 // 引入 routes 
 import routes from './config/routes.js'
-import Login from './components/login/login'
+import Login from './components/Login/Login'
 import Admin from './components/admin/admin'
 // 引入 css
 // 引入最外面的大组件 BasicLayout组件
--- acg-project/acg-goddess/src/redux/store.js
+++ acg-project/acg-goddess/src/redux/store.js
@@ -1,9 +1,10 @@
-import {createStore,applyMiddleware} from 'redux'
+// 引入redux
+import { createStore, applyMiddleware } from "redux"
 // 引入thunk
-import thunk from 'redux-thunk'
-// 引入redux-devtools-extension
+import thunk from "redux-thunk"
+// 引入redux-DevTools-extension
 import {composeWithDevTools} from 'redux-devtools-extension'
 // 引入reducers
 import reducers from './reducers.js'
-// 异步操作 --- thunk Applymiddleware
+// 暴露
 export default createStore(reducers,composeWithDevTools(applyMiddleware(thunk)))
